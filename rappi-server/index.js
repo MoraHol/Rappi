f80449cc3edbf078c99134925ebd217bddf6b4a2
Alexis Holguin <wholguinmor@uniminuto.edu.co>
2019-03-31 13:54:03 -0500

    maqueta de rappitendero

--- rappi-server/index.js
+++ rappi-server/index.js
@@ -4,7 +4,7 @@ const express = require('express')
 const app = express()
 const router = require('./routes')
 
-app.use('/public', express.static('public'))
+app.use(express.static('public'))
 app.use('/', router)
 
 app.listen(process.env.PORT || 3000, () => {
--- rappi-server/routes.js
+++ rappi-server/routes.js
@@ -7,5 +7,11 @@ router.get('/login', (req, res) => {
   res.type('html')
   res.sendFile(path.resolve(`${__dirname}/public/loginClient.html`))
 })
-
+router.get('/soyrappi', (req, res) => {
+  res.type('html')
+  res.sendFile(path.resolve(`${__dirname}/public/loginRT.html`))
+})
+router.get('/', (req, res) => {
+  res.send('')
+})
 module.exports = router
