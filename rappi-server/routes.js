e2c91aad8506f09af4034d7abc8511b9c4510fb8
Alexis Holguin <wholguinmor@uniminuto.edu.co>
2019-04-01 15:23:06 -0500

    formularios de registros de usuario y rappi tendero terminadas rutas especificadas de prueba

--- rappi-server/routes.js
+++ rappi-server/routes.js
@@ -1,4 +1,5 @@
 'use strcit'
+
 const express = require('express')
 const path = require('path')
 const router = express.Router()
@@ -7,11 +8,21 @@ router.get('/login', (req, res) => {
   res.type('html')
   res.sendFile(path.resolve(`${__dirname}/public/loginClient.html`))
 })
+
 router.get('/soyrappi', (req, res) => {
   res.type('html')
   res.sendFile(path.resolve(`${__dirname}/public/loginRT.html`))
 })
+router.get('/formClient', (req, res) => {
+  res.type('html')
+  res.sendFile(path.resolve(`${__dirname}/public/formClient.html`))
+})
+router.get('/formRT', (req, res) => {
+  res.type('html')
+  res.sendFile(path.resolve(`${__dirname}/public/formRT.html`))
+})
 router.get('/', (req, res) => {
   res.send('')
 })
+
 module.exports = router
