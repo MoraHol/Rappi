2bc4c300a528d9eac7a676249b019d99f17cfea9
sebasb718 <sebasb718@gmail.com>
2019-04-04 20:41:00 -0500

    Creación archivos para BD

---
+++ rappi-server/migrations/20190404203253_create_users.js
@@ -0,0 +1,20 @@
+
+exports.up = function(knex, Promise) {
+    return Promise.all([
+        knex.schema.createTable('users', table => {
+          table.increments('id').primary()
+          table.string('email')
+          table.string('first_name')
+          table.string('last_name')
+          table.string('googleid')
+          table.string('address')
+          table.string('address_details')
+        })
+      ])
+};
+
+exports.down = function(knex, Promise) {
+    return Promise.all([
+        knex.schema.dropTable('ideas')
+      ])
+};
